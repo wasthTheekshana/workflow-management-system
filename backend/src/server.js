58bb354159ec5b28498a4547f96b1481600da37f
wasthTheekshana <harishjonemike30@gmail.com>
2026-08-28 12:48:18 +0530

    feat: start notification worker on server boot

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 const app = require('./app');
 const { validateEnv } = require('./config/env');
+const { startNotificationWorker } = require('./workers/notificationWorker');
 
 const config = validateEnv();
 
 app.listen(config.port, () => {
   console.log(`Workflow engine API listening on port ${config.port}`);
 });
+
+startNotificationWorker();

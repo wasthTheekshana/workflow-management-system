db85dfdc338636498637b45df013c8eb86cc2aa9
wasthTheekshana <harishjonemike30@gmail.com>
2026-08-28 18:46:07 +0530

    chore: scaffold frontend (Vite, React, TypeScript, Tailwind)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,50 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { AdminRoute } from './components/AdminRoute';
+import { Layout } from './components/Layout';
+import { LoginPage } from './pages/LoginPage';
+import { MyTasksPage } from './pages/MyTasksPage';
+import { NewInstancePage } from './pages/NewInstancePage';
+import { InstanceDetailPage } from './pages/InstanceDetailPage';
+import { TemplateFilesPage } from './pages/admin/TemplateFilesPage';
+import { TemplateFileDetailPage } from './pages/admin/TemplateFileDetailPage';
+import { WorkflowTemplatesPage } from './pages/admin/WorkflowTemplatesPage';
+import { WorkflowTemplateDetailPage } from './pages/admin/WorkflowTemplateDetailPage';
+import { DocumentTypesPage } from './pages/admin/DocumentTypesPage';
+import { AdminInstancesPage } from './pages/admin/AdminInstancesPage';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<ProtectedRoute />}>
+              <Route element={<Layout />}>
+                <Route path="/my-tasks" element={<MyTasksPage />} />
+                <Route path="/instances/new" element={<NewInstancePage />} />
+                <Route path="/instances/:id" element={<InstanceDetailPage />} />
+                <Route element={<AdminRoute />}>
+                  <Route path="/admin/template-files" element={<TemplateFilesPage />} />
+                  <Route path="/admin/template-files/:id" element={<TemplateFileDetailPage />} />
+                  <Route path="/admin/workflow-templates" element={<WorkflowTemplatesPage />} />
+                  <Route path="/admin/workflow-templates/:id" element={<WorkflowTemplateDetailPage />} />
+                  <Route path="/admin/document-types" element={<DocumentTypesPage />} />
+                  <Route path="/admin/instances" element={<AdminInstancesPage />} />
+                </Route>
+              </Route>
+            </Route>
+            <Route path="*" element={<Navigate to="/my-tasks" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
